b3c7329fa394d57b8289dc572e66cb3c8aa8dca2
Deepa Mariam George <60174258+ceccs18c14@users.noreply.github.com>
2020-03-21 13:58:08 +0530

    t8 (ii) completed

---
+++ 2103/odd-even.pl
@@ -0,0 +1,12 @@
+use warnings;
+use strict;
+print "Enter a number:";
+$a=<>;
+if (0==$a % 2)
+{ 
+ print "Given number is even \n";
+}
+else
+{
+ print "Given number is odd \n";
+}
---
+++ 2103/sum2num.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/perl
+
+print "\nEnter two numbers: ";
+
+# read elements
+$x = <>;
+$y = <>;
+
+# compute sum
+$z = $x +$y;
+
+# print sum
+print "\nSum is $z\n"; 
